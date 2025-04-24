f121c727b50a27ea9db2fb90cb9a9a326294fe10
Ankan Pal <itsyourap@gmail.com>
2025-04-24 23:15:50 +0530

    Add C17: Omnitrix Core Breach challenge

---
+++ C17/omnitrix.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void transform_into_xlr8()
+{
+  printf("Accessing Omnitrix Core...\n");
+  FILE *fptr;
+  char c;
+
+  fptr = fopen("/flag.txt", "r");
+  if (fptr == NULL)
+  {
+    printf("Cannot open file.\n");
+    exit(0);
+  }
+
+  c = fgetc(fptr);
+  while (c != EOF)
+  {
+    printf("%c", c);
+    c = fgetc(fptr);
+  }
+
+  printf("\n");
+  fclose(fptr);
+}
+
+void interface()
+{
+  char name[64];
+  printf("State your name, wielder of the Omnitrix: ");
+  fgets(name, 1024, stdin);
+  printf("Welcome, %s\n", name);
+}
+
+int main()
+{
+  printf("=== Omnitrix Authorization Interface ===\n");
+  interface();
+  printf("Authorization Failed. Alien transformation denied.\n");
+  return 0;
+}
